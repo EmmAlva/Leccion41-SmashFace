088444e59091a62542423713c54b832c3d013ae9
EmmAlva <clioemma_28@hotmail.com>
2017-06-12 12:28:53 -0500

    aleatorio

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
-/*$.getScript('js/mexico.js'), function(){
-	var resultado = mexico[0].name(1);
-}*/
-
-/*var photo = $('#photo-coder');
-
-photo.attr("src") = mexico.imagen; */
 'use strict';
 
-
-/*const wrapper = $('<div class ="wrapper"></div>');
-const container = $('<div class="codersSection"></div>');
-const img = $('<img>'+state.selectedSede.imagen);
-*/
 const state = {
 		sede1: mexico,
 		sede2: peru,
@@ -20,21 +8,33 @@ const state = {
 const namePeru = state.sede2;
 const nameMexico = state.sede1;
 
+$.each(mexico, function(index, elem){
+
+})
+
 $(document).ready(function(){	
+	const imgRandoMex = mexico[Math.floor(Math.random()*mexico.length)];
+	const imgRandoPer = peru[Math.floor(Math.random()*peru.length)];
+	console.log(imgRandoPer);
+	console.log(imgRandoMex);
 
 	$(".sedesLab").change(function(){
 		if($(".sedesLab").val() == "mexico"){
-			$(".photo-coder").attr("src","img/mexico/"+mexico[0].image);
+			$(".photo-coder").attr("src","img/mexico/"+ imgRandoMex.image);
 		}
 		else if($(".sedesLab").val() == "lima" || $(".sedesLab").val() == "arequipa"){
-			$(".photo-coder").attr("src","img/peru/"+peru[4].image);
+			$(".photo-coder").attr("src","img/peru/"+imgRandoPer.image);
 		}
 		else if($(".sedesLab").val() == "chile"){
 			$(".photo-coder").parent().text("Sin datos, prueba de nuevo");
 		}
 
+	
 	$(".btn").click(function(){
-		if($("#name").val() == namePeru[7].name){
+		if($("#name").val() !== imgRandoMex.name){
+			alert("try again");
+		}
+		else if ($("#name").val() !== imgRandoPer.name){
 			alert("try again");
 		};
 	});
